f0b17131ba60694fe4cc99b0146f2de084fe3b30
[ Cassondra ] <carobert@redhat.com>
2021-02-27 15:49:42 -0500

    Adding UX enhancements

--- js/home.js
+++ js/home.js
@@ -23,11 +23,18 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+const getNavHeight = () => {
+  let navHeight = 0;
+  const nav = document.querySelector("#social");
+  if (nav) navHeight = nav.getBoundingClientRect().height;
+  return navHeight;
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   Promise.all([
     customElements.whenDefined("pfe-accordion"),
     customElements.whenDefined("pfe-tabs")
-  ]).then(function() {
+  ]).then(function () {
     document.querySelectorAll("pfe-accordion").forEach((accordion, count) => {
       accordion.disclosure = "true";
       if (count === 0) accordion.expand(0);
@@ -41,26 +48,63 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
 
+    let navHeight = getNavHeight();
     const tabs = document.querySelector("pfe-tabs");
     if (tabs) {
       const shadowTabs = tabs.shadowRoot.querySelector(".tabs");
       tabs.style.alignItems = "flex-start";
       if (shadowTabs) {
         shadowTabs.style.position = "sticky";
-        shadowTabs.style.top = "75px";
+        shadowTabs.style.top = `var(--navigation-height, ${navHeight}px)`;
+        shadowTabs.style.backgroundColor = "#fff";
+        shadowTabs.style.zIndex = "98";
       }
     }
   });
+
+  // Check for query param
+  const urlParams = new URLSearchParams(window.location.search);
+  const displayMode = urlParams.get("format");
+  if (displayMode === "cv") {
+    toggleCV("add");
+    document.querySelector("#cvToggle").checked = true;
+  }
 });
+
+// Update height on resize
+window.addEventListener('resize', () => {
+  let navHeight = getNavHeight();
+  const tabs = document.querySelector("pfe-tabs");
+  if (tabs) {
+    const shadowTabs = tabs.shadowRoot.querySelector(".tabs");
+    if (shadowTabs) document.body.style.setProperty("--navigation-height", `${navHeight}px`);
+  }
+});
+
 
 // pfe-tab:shown-tab
 document.addEventListener("pfe-tabs:shown-tab", evt => {
+  const mq = window.matchMedia("(min-width: 992px)");
   if (evt && evt.detail && evt.detail.tab) {
     const tab = evt.detail.tab;
     const panel = tab.nextElementSibling;
     // Activate the first accordion
     const accordion = panel.querySelector("pfe-accordion");
-    if (accordion) setTimeout(accordion.expand(0), 500);
+    let navHeight = getNavHeight();
+    // Scroll into view
+    var elementPosition = accordion.getBoundingClientRect().top - document.body.getBoundingClientRect().top;
+    if (!mq.matches) navHeight = navHeight + tab.getBoundingClientRect().height;
+    if (accordion) {
+      window.scrollTo({
+        top: elementPosition - navHeight,
+        behavior: "smooth"
+      });
+
+      setTimeout(() => {
+        accordion.expand(0);
+      }, 500);
+      // else accordion.collapseAll();
+    }
   }
 });
 
@@ -78,10 +122,38 @@ document.querySelectorAll(".read-more").forEach(link => {
   });
 });
 
-const toggleCV = () => {
+const updateQuery = (params, add = true) => {
+  const url = new URL(window.location);
+  params.map(param => {
+    if (add) url.searchParams.set(param.key, param.value);
+    else url.searchParams.delete(param.key);
+  });
+  window.history.pushState({}, '', url);
+}
+
+const toggleCV = (state = "toggle") => {
   document.body.classList.toggle("animating");
-  document.body.classList.toggle("cv");
+  // Toggle the CV class on the body
+  document.body.classList[state]("cv");
+
+  // Apply the CV query string
+  if (document.body.classList.contains("cv")) {
+    updateQuery([{
+      key: "format",
+      value: "cv"
+    }]);
+  } else {
+    updateQuery([{
+      key: "format"
+    }], false);
+  }
+
+  // Reset the context on bands and cards
   document.querySelectorAll("pfe-band,pfe-card").forEach(component => component.resetContext());
+
+  // Close the open accordions
+  document.querySelectorAll("pfe-accordion").forEach(accordion => accordion.collapseAll());
+
   setTimeout(() => {
     document.body.classList.toggle("animating");
   }, 300);
