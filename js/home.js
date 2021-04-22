d2155b365d9faaffea51f1c444d86245dcda6849
[ Cassondra ] <castastrophe@users.noreply.github.com>
2021-04-22 15:54:35 -0400

    Uncomment home.js

--- js/home.js
+++ js/home.js
@@ -1,27 +1,29 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   $('[data-popup="youtube"]').magnificPopup({
-//     disableOn: 600,
-//     type: 'iframe',
-//     mainClass: 'mfp-fade',
-//     removalDelay: 160,
-//     preloader: false,
-//     fixedContentPos: false
-//   });
+document.addEventListener("DOMContentLoaded", function () {
+  try {
+    $('[data-popup="youtube"]').magnificPopup({
+      disableOn: 600,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false
+    });
 
-//   $('.popup-link').magnificPopup({
-//     disableOn: 600,
-//     type: 'image',
-//     closeOnContentClick: 'true',
-//     zoom: {
-//       enabled: true,
-//       duration: 300,
-//       ease: "ease-in-out",
-//       opener: function (openerElement) {
-//         return openerElement.is('img') ? openerElement : openerElement.find('img');
-//       }
-//     }
-//   });
-// });
+    $('.popup-link').magnificPopup({
+      disableOn: 600,
+      type: 'image',
+      closeOnContentClick: 'true',
+      zoom: {
+        enabled: true,
+        duration: 300,
+        ease: "ease-in-out",
+        opener: function (openerElement) {
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+  } catch (err) {}
+});
 
 const getNavHeight = () => {
   let navHeight = 0;
