ece067c64ba7fa1da9f2e6674d973d7934a7ebb6
[ Cassondra ] <castastrophe@users.noreply.github.com>
2025-10-26 14:28:11 -0400

    feat: implementing strict webc (#16)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,74 +1,132 @@
 import path from "node:path";
+import { writeFile } from "node:fs/promises";
 import postcss from "postcss";
 import loadConfig from "postcss-load-config";
+import { minify } from "html-minifier";
 
 import pluginWebc from "@11ty/eleventy-plugin-webc";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import { InputPathToUrlTransformPlugin } from "@11ty/eleventy";
 
-/** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
-export default async function(eleventyConfig) {
-	eleventyConfig.ignores.add("README.md");
+/** @param {import('@11ty/eleventy')} config */
+export default async function(config) {
+	// Layout aliases make templates more portable.
+	config.addLayoutAlias("base", "layouts/base.webc");
 
-    eleventyConfig.addPlugin(pluginWebc, {
+	config.ignores.add("README.md");
+
+	config.addPlugin(pluginWebc, {
 		// Glob to find no-import global components
-		// (The default changed from `false` in Eleventy WebC v0.7.0)
-		components: "./_components/**/*.webc",
+		components: "./_includes/components/*.webc",
 
 		// Adds an Eleventy WebC transform to process all HTML output
-		useTransform: false,
+		useTransform: true,
 
 		// Additional global data used in the Eleventy WebC transform
 		transformData: {},
 
 		// Options passed to @11ty/eleventy-plugin-bundle
 		bundlePluginOptions: {},
-    });
+	});
 
-    eleventyConfig.addTemplateFormats("scss");
-    eleventyConfig.addExtension("scss", {
-		outputFileExtension: "css",
+	const styles = await loadConfig({ env: process.env.ELEVENTY_ENV });
+	const processCSS = async function (content, inputPath, outputPath) {
+		const parsed = path.parse(inputPath);
+		console.log(outputPath, parsed.name);
 
-		// opt-out of Eleventy Layouts
-		// useLayouts: false,
-
-		compile: async function (inputContent, inputPath) {
-			let parsed = path.parse(inputPath);
-			// Don’t compile file names that start with an underscore
-			if(parsed.name.startsWith("_")) {
-				return;
+		return postcss(styles.plugins).process(content, {
+			...styles.options,
+			from: inputPath,
+			to: outputPath
+		}).then(async result => {
+			// Write the map file to the output directory
+			if (result.map) {
+				await writeFile(path.join(path.dirname(outputPath), "css", parsed.name + ".css.map"), result.map.toString());
 			}
 
-            const config = await loadConfig();
-            const result = await postcss(config.plugins).process(inputContent);
+			return result.css;
+		});
+	};
 
-			// Map dependencies for incremental builds
-			this.addDependencies(inputPath, result.loadedUrls);
+	config.addBundle("css", {
+		toFileDirectory: "css",
+		transforms: [
+			async function(content) {
+				return processCSS(content, this.inputPath, this.page?.outputPath);
+			}
+		]
+	});
 
-			return async (data) => {
-				return result.css;
+	config.addTemplateFormats('css');
+	config.addExtension('css', {
+		outputFileExtension: 'css',
+		compile: async (inputContent, inputPath) => {
+			return async ({ page }) => {
+				return processCSS(inputContent, inputPath, page?.outputPath);
 			};
 		},
 	});
 
-	eleventyConfig.addCollection("posts", function(collectionApi) {
-		return collectionApi.getFilteredByGlob("pages/posts/**/*");
+	config.addCollection("posts", function(collectionApi) {
+		return collectionApi.getFilteredByGlob("pages/posts/*.md");
 	});
 
-	eleventyConfig.addPassthroughCopy("img");
+	config.addPassthroughCopy("img");
 
-	eleventyConfig.addPlugin(InputPathToUrlTransformPlugin);
+	config.addPlugin(InputPathToUrlTransformPlugin);
+	config.addPlugin(syntaxHighlight);
 
-	eleventyConfig.setServerOptions({
+	config.addFilter("isPost", function(page) {
+		return page.inputPath.includes("posts/");
+	});
+
+	config.addFilter("toISOString", function(date) {
+		return date?.toISOString();
+	});
+
+	config.addPassthroughCopy({
+		"node_modules/prismjs/themes/prism.css": "css/prism.css",
+		"node_modules/prism-themes/themes/prism-one-light.css": "css/prism-one-light.css",
+		"node_modules/prism-themes/themes/prism-one-dark.css": "css/prism-one-dark.css",
+		"node_modules/prismjs/prism.js": "js/prism.js",
+		"pages/home.css": "css/home.css",
+		"pages/home.js": "js/home.js"
+	});
+
+	config.setServerOptions({
+		// Open the browser automatically
+		open: true,
+		browser: "firefox",
 		domDiff: false
 	});
 
+	// A debug utility.
+	config.addFilter("dump", obj => {
+	  return util.inspect(obj);
+	});
+
+	if (process.env.ELEVENTY_ENV === 'production') {
+		config.addTransform('htmlmin', (content, outputPath) => {
+		  if (!outputPath.endsWith('.html')) return content;
+
+			return minify(content, {
+				collapseInlineTagWhitespace: false,
+				collapseWhitespace: true,
+				removeComments: true,
+				sortClassName: true,
+				useShortDoctype: true,
+			});
+		});
+	  }
+
 	return {
-        dir: {
-            input: "pages",
-            output: "public",
-            includes: "../_includes",
-            data: "../_data",
-        },
+		dir: {
+			input: "pages",
+			output: "public",
+			includes: "../_includes",
+			data: "../_data",
+		},
 		markdownTemplateEngine: false,
-    };
+		htmlTemplateEngine: "webc",
+	};
 };
